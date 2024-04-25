3e327d05516bd155d712fc6a7cc7bf8641272996
NissaFaber <57180375+NissaFaber@users.noreply.github.com>
2024-04-25 16:07:09 +0200

    Update model.py

--- src/lib/models/model.py
+++ src/lib/models/model.py
@@ -37,7 +37,7 @@ def load_model(model, model_path, optimizer=None, resume=False,
   start_epoch = 0
   checkpoint = torch.load(model_path, map_location=lambda storage, loc: storage)
   print('loaded {}, epoch {}'.format(model_path, checkpoint['epoch']))
-  state_dict_ = checkpoint['state_dict']
+  state_dict_ = checkpoint
   state_dict = {}
   
   # convert data_parallal to model
