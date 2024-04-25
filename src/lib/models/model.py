3258d33f3bf13f8986e4d86365605a8297e7977f
NissaFaber <57180375+NissaFaber@users.noreply.github.com>
2024-04-25 18:01:53 +0200

    Update model.py

--- src/lib/models/model.py
+++ src/lib/models/model.py
@@ -7,22 +7,22 @@ import torch
 import torch.nn as nn
 import os
 
-from .networks.dlav0 import get_pose_net as get_dlav0
-from .networks.pose_dla_dcn import get_pose_net as get_dla_dcn
-from .networks.resnet_dcn import get_pose_net as get_pose_net_dcn
-from .networks.resnet_fpn_dcn import get_pose_net as get_pose_net_fpn_dcn
+# from .networks.dlav0 import get_pose_net as get_dlav0
+# from .networks.pose_dla_dcn import get_pose_net as get_dla_dcn
+# from .networks.resnet_dcn import get_pose_net as get_pose_net_dcn
+# from .networks.resnet_fpn_dcn import get_pose_net as get_pose_net_fpn_dcn
 from .networks.pose_hrnet import get_pose_net as get_pose_net_hrnet
-from .networks.pose_dla_conv import get_pose_net as get_dla_conv
-from .yolo import get_pose_net as get_pose_net_yolo
+# from .networks.pose_dla_conv import get_pose_net as get_dla_conv
+# from .yolo import get_pose_net as get_pose_net_yolo
 
 _model_factory = {
-  'dlav0': get_dlav0, # default DLAup
-  'dla': get_dla_dcn,
-  'dlaconv': get_dla_conv,
-  'resdcn': get_pose_net_dcn,
-  'resfpndcn': get_pose_net_fpn_dcn,
+  # 'dlav0': get_dlav0, # default DLAup
+  # 'dla': get_dla_dcn,
+  # 'dlaconv': get_dla_conv,
+  # 'resdcn': get_pose_net_dcn,
+  # 'resfpndcn': get_pose_net_fpn_dcn,
   'hrnet': get_pose_net_hrnet,
-  'yolo': get_pose_net_yolo
+  # 'yolo': get_pose_net_yolo
 }
 
 def create_model(arch, heads, head_conv):
@@ -35,8 +35,8 @@ def create_model(arch, heads, head_conv):
 def load_model(model, model_path, optimizer=None, resume=False, 
                lr=None, lr_step=None):
   start_epoch = 0
-  checkpoint = torch.load(model_path, map_location=lambda storage, loc: storage)
-  print('loaded {}, epoch {}'.format(model_path, checkpoint['epoch']))
+  checkpoint = torch.load(model_path,)
+  # print('loaded {}, epoch {}'.format(model_path, checkpoint['epoch']))
   state_dict_ = checkpoint
   state_dict = {}
   
